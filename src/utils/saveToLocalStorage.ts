739df4352694d7b56d27a67a25fdf28992a06dfd
Danil <danylo.makhov@nure.ua>
2023-05-04 22:33:57 +0300

    fix: fix saving data to localstorage

--- src/utils/saveToLocalStorage.ts
+++ src/utils/saveToLocalStorage.ts
@@ -1,11 +1,42 @@
 import { todoStatus } from '../types/reponseType/responseType';
-import { todoType } from '../types/store/reducersTypes/repoReducerTypes';
+import { initialRepoType, todoType } from '../types/store/reducersTypes/repoReducerTypes';
 
 const saveIssuesToLocalStorage = (todos: todoType[], gitUrl: string) => {
-  const filteredIssues = todos.filter((todo) => todo.todoStatus !== todoStatus.TODO);
-  const resultIssues = [...filteredIssues
-    .flatMap((issue) => issue.issues
-      .map((issueItem) => ({ issueStatus: issue.todoStatus, id: issueItem.id, gitUrl })))];
-  localStorage.setItem('issues', JSON.stringify(resultIssues));
+  const localStorageIssues = localStorage.getItem('issues');
+  if (localStorageIssues) {
+    const antotherGithubIssues = (JSON.parse(localStorageIssues) as initialRepoType[])
+      .filter((issue) => issue.repoUrl !== gitUrl);
+    if (antotherGithubIssues.length > 0) {
+      const filteredIssues = todos.filter((todo) => todo.todoStatus !== todoStatus.TODO);
+      const resultIssues = [...filteredIssues
+        .flatMap((issue) => issue.issues
+          .map((issueItem) => ({
+            issueStatus: issue.todoStatus,
+            id: issueItem.id,
+            repoUrl: gitUrl,
+          })))];
+      localStorage.setItem('issues', JSON.stringify([...antotherGithubIssues, ...resultIssues]));
+    } else {
+      const filteredIssues = todos.filter((todo) => todo.todoStatus !== todoStatus.TODO);
+      const resultIssues = [...filteredIssues
+        .flatMap((issue) => issue.issues
+          .map((issueItem) => ({
+            issueStatus: issue.todoStatus,
+            id: issueItem.id,
+            repoUrl: gitUrl,
+          })))];
+      localStorage.setItem('issues', JSON.stringify(resultIssues));
+    }
+  } else {
+    const filteredIssues = todos.filter((todo) => todo.todoStatus !== todoStatus.TODO);
+    const resultIssues = [...filteredIssues
+      .flatMap((issue) => issue.issues
+        .map((issueItem) => ({
+          issueStatus: issue.todoStatus,
+          id: issueItem.id,
+          repoUrl: gitUrl,
+        })))];
+    localStorage.setItem('issues', JSON.stringify(resultIssues));
+  }
 };
 export default saveIssuesToLocalStorage;
